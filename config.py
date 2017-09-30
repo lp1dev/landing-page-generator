362912c332e3075b46a40722ee5c16c9ed9d589b
lp1 <mail@lp1.eu>
2017-09-30 14:02:57 +0200

    first working version

---
+++ config.py
@@ -0,0 +1,16 @@
+## Hugo configuration
+theme = "vertical-land"
+theme_repo = "https://github.com/lp1dev/vertical-land.git" ## Theme repository
+hugo_config = "config.toml" ## Hugo config file
+hugo_config_template = """
+baseurl = "https://lp1.eu/%s"
+languageCode = "en-us"
+title = "%s"
+theme = "vertical-land"
+
+[params]
+description = "%s"
+mainTitle = "%s"
+subTitle = "%s"
+optinTitle = "%s"
+background = "%s" """
--- generator.py
+++ generator.py
@@ -1,11 +1,50 @@
 #!/bin/env python
 
+from    subprocess import call
+import  json
+import  config
+
 class   Page():
   def   __init__(self, name):
     self.name = name
+    self.shortName = self.name.strip()
+    self.relpath = "static/" + self.shortName
+    
+  def   prepare(self):
+    call(["hugo", "new", "site", self.relpath])
+    call(["git", "clone", config.theme_repo, self.relpath + "/themes/" + config.theme])
+    return
+
+  def   configure(self, header, logo, services, optin, footer):
+    with open(self.relpath + "/" + config.hugo_config, "w+") as f:
+      hugo_config = config.hugo_config_template %(
+        self.shortName,
+        self.name,
+        self.name,
+        header,
+        self.name,
+        self.name,
+        optin
+      )
+      f.write(hugo_config)
+    with open(self.relpath + "/data/footer.json", "w+") as f:
+      f.write(json.dumps(footer))
+    with open(self.relpath + "/data/services.json", "w+") as f:
+      f.write(json.dumps(services))
+
+  def   deploy(self):
     return
 
 def	main():
+  page = Page("Test Page")
+  page.prepare()
+  page.configure("This is my generated page",
+                 "https://lp1.eu/public/amsell_j.png",
+                 {"services": [{"title":"Test"}]},
+                 "",
+                 {"footer":"Hello I'm the footer"}
+  )
+  page.deploy()
   return
 
 if __name__ == '__main__':
